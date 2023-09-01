370cc8e4012d3f27c2d363cf666982d4108c5106
sonam <sonamporwal.wiz91@gmail.com>
2023-09-01 18:36:59 +0530

    Created freelancer profile(home) page & created functionality of navbar after login

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../images/Alanced.png'
 import { Link } from 'react-router-dom'
 import navback from '../images/Nav_Background.png'
 import { useDispatch, useSelector } from 'react-redux'
 import { LogoutAction } from '../../redux/Auth/AuthAction'
+import search from '../images/SearchOutlined.png'
+import searchbtn from '../images/searchbtn.png'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import profilepic from '../../components/images/profilepic.png'
 
 const Navbar = () => {
   
@@ -13,6 +17,7 @@ const Navbar = () => {
   const googleUserName = localStorage.getItem('googleUserName');
   const loginMethod = localStorage.getItem('loginMethod');
   const dispatch = useDispatch();
+  const [dropdownVisible, setDropdownVisible] = useState(false);
  
   let displayName;
 
@@ -36,7 +41,7 @@ const Navbar = () => {
 
 
   return (
-    <div className='sticky z-50 top-0 bg-cover bg-top' style={{ backgroundImage: `url(${navback})`}}>
+<div className='sticky z-50 top-0 bg-cover bg-top' style={{ backgroundImage: `url(${navback})`}}>
       <nav class="flex items-center justify-between flex-wrap p-6">
   <Link to='/'>
   <div class="flex items-center flex-shrink-0 lg:ml-[129px]">
@@ -44,57 +49,112 @@ const Navbar = () => {
     <span class="font-semibold text-[23px] tracking-widest ml-2 font-poppins text-[#031136]">ALANCED</span>
   </div>
   </Link>
-  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto mt-0">
+  {!isLoggedIn ? 
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto mt-0">
     <div class="text-sm lg:flex-grow lg:ml-[45px]">
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 lg:mr-12 font-inter text-[16px] text-[#031136]">
+      <Link to=''><span class="block mt-4 lg:inline-block lg:mt-0 lg:mr-12 font-inter text-[16px] text-[#031136]">
         Find Talent
-      </a>
-      <a class="block mt-4 lg:inline-block lg:mt-0 lg:mr-12 font-inter text-[16px] text-[#031136]">
-        <Link to='/projects'>Find Work</Link>
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 lg:mr-12 font-inter text-[16px] text-[#031136]">
+      </span></Link>
+      <Link to='/projects'><span class="block mt-4 lg:inline-block lg:mt-0 lg:mr-12 font-inter text-[16px] text-[#031136]">Find Work</span></Link>
+      <Link to=''><span class="block mt-4 lg:inline-block lg:mt-0 lg:mr-12 font-inter text-[16px] text-[#031136]">
         Why Alanced
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 font-inter text-[16px] text-[#031136]">
+      </span></Link>
+      <Link to=''><span class="block mt-4 lg:inline-block lg:mt-0 font-inter text-[16px] text-[#031136]">
         Enterprise
-      </a>
+      </span></Link>
     </div>
     <div className='lg:mr-[100px] mt-2'>
-      {!isLoggedIn ? (
-        <>
         <Link to='/login'><span class="inline-block text-sm px-4 py-[10px] mt-4 lg:mt-0 bg-gradient-to-r from-[#00BF58] to-[#E3FF75] border rounded border-none text-white mr-2 font-semibold">Sign In</span></Link>
       <div class="p-0.5 inline-block rounded bg-gradient-to-b from-[#00BF58] to-[#E3FF75]">
       <Link to='/choose'><button class="px-2 py-1 bg-[#E2F9EE]"><p class="bg-gradient-to-r from-primary to-danger bg-clip-text text-transparent font-semibold text-sm py-[4px] px-[6px]">Sign Up</p></button></Link>
-</div>
-        </>
-      ):(<>
-      <span className='pr-3 font-cardo text-xl'>Welcome, 
-  {loginType === 'FREELANCER'? (
-    <Link to="/freelancer/profile" className="font-bold">{" "+displayName}</Link>
-  ) : (
-      <Link to="/projects" className="font-bold">{" "+displayName}</Link>
-    ) 
-  }
-</span>
-        {/* <span className='pr-3 font-cardo text-xl'>Welcome, <span className="font-bold">{displayName}</span></span> */}
-        {/* <Link to='/login'><span class="inline-block text-sm px-4 py-[10px] mt-4 lg:mt-0 bg-gradient-to-r from-[#00BF58] to-[#E3FF75] border rounded border-none text-white mr-2 font-semibold" onClick={() => {
-          localStorage.removeItem('isLoggedIn');
-          localStorage.removeItem('googleUserName');
-          localStorage.removeItem('accessToken');
-          localStorage.removeItem('loginMethod');
-          }}>Logout</span></Link> */}
-          <Link to='/'>
-        <span 
-            class="inline-block text-sm px-4 py-[10px] mt-4 lg:mt-0 bg-gradient-to-r from-[#00BF58] to-[#E3FF75] border rounded border-none text-white mr-2 font-semibold" 
-            onClick={handleLogout}
-        >
-            Logout
-        </span>
-    </Link>
-      </>)}
-      
+</div>    
     </div>
+  </div> :
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto mt-0">
+    <div class="text-sm lg:flex-grow lg:ml-[45px]">
+      <Link to='/projects'><span href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 lg:mr-12 font-inter text-[16px] text-[#031136]">Find Work</span></Link>
+      <Link to=''><span class="block mt-4 lg:inline-block lg:mt-0 lg:mr-12 font-inter text-[16px] text-[#031136]">My Jobs</span></Link>
+      <Link to=''><span class="block mt-4 lg:inline-block lg:mt-0 lg:mr-12 font-inter text-[16px] text-[#031136]"> Reports</span></Link>  
+      <Link to=''><span href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 font-inter text-[16px] text-[#031136]">Messages</span></Link>
+    </div>
+    <section className='flex items-center'>
+    <div className='flex items-center p-2 bg-white rounded-lg shadow-md'>
+        <div className='flex items-center mr-1'>
+            <img src={search} alt="Search Icon" className="h-3 w-3 mr-1" />
+            <input className='w-40 lg:w-56 h-7 text-xs lg:text-sm rounded mr-1' placeholder='Search' />
+        </div>
+        <div className="border-l h-5 mx-5 border-gray-400"></div>
+        <div className='mr-1'>
+            <select id="countries" class="bg-gray-50 text-[#797979] text-xs font-inter font-normal rounded h-7 mr-1 cursor-pointer">
+                <option selected>Talent</option>
+                <option value="">Project</option>
+                <option value="">Jobs</option>
+            </select>
+        </div>
+        <button className='rounded h-7 w-7 p-2 text-xs lg:text-sm font-semibold text-white bg-gradient-to-r from-[#00BF58] to-[#E3FF75]'>
+            <img src={searchbtn} alt="Search Icon" className="" />
+        </button>
+    </div>
+</section>
+
+<div className="ml-4 relative">
+    <i class="bi bi-bell text-xl"></i>
+    <span class="absolute top-0.5 right-0 block h-2.5 w-2.5 rounded-full bg-green-500 border-2 border-white"></span>
+</div>
+
+<div className="ml-4 relative">
+{logindata.images_logo !== "/media/images/blank.png" ? (
+        <img 
+            src={"https://aparnawiz91.pythonanywhere.com/" + logindata.images_logo} 
+            alt="Profile" 
+            className="h-8 w-8 rounded-full cursor-pointer" 
+            onClick={() => setDropdownVisible(!dropdownVisible)}
+        />
+    ) : (
+        <div 
+            className="h-8 w-8 rounded-full cursor-pointer flex items-center justify-center bg-gradient-to-r from-[#00BF58] to-[#E3FF75] text-white font-bold font-cardo text-xl p-1"
+            onClick={() => setDropdownVisible(!dropdownVisible)}
+        >
+            {logindata.first_Name && logindata.first_Name[0].toUpperCase()}
+        </div>
+    )}
+                {/* <img 
+                    src={"https://aparnawiz91.pythonanywhere.com/"+logindata.images_logo} 
+                    alt="Profile" 
+                    className="h-9 w-9 rounded-full cursor-pointer" 
+                    onClick={() => setDropdownVisible(!dropdownVisible)}
+                /> */}
+                {dropdownVisible && (
+                  <div className="absolute right-0 mt-5 w-48 rounded-md shadow-lg bg-white">
+                  <div className="py-1">
+                      <Link to='/freelancer/profile' className="flex items-center px-4 py-2 hover:bg-gray-100">
+                          <i class="bi bi-person mr-3 opacity-50"></i>
+                          <span className="font-inter text-[16px] text-[#031136] opacity-50">My Profile</span>
+                      </Link>
+                      <Link to='/freelancer/editprofile' className="flex items-center px-4 py-2 hover:bg-gray-100">
+                          <i class="bi bi-pencil mr-3 opacity-50"></i>
+                          <span className="font-inter text-[16px] text-[#031136] opacity-50">Edit Profile</span>
+                      </Link>
+                      <Link to='/' className="flex items-center px-4 py-2 hover:bg-gray-100" onClick={handleLogout}>
+                          <i class="bi bi-box-arrow-right mr-3 opacity-50"></i>
+                          <span className="font-inter text-[16px] text-[#031136] opacity-50">Logout</span>
+                      </Link>
+                  </div>
+              </div>
+              
+                    // <div className="absolute right-0 mt-5 w-48 rounded-md shadow-lg bg-white">
+                    //     <div className="py-1">
+                    //         <Link to='/freelancer/profile'><span className="block px-4 py-2 font-inter text-[16px] text-[#031136] hover:bg-gray-100">My Profile</span></Link>
+                    //         <Link to='/freelancer/editprofile'><span className="block px-4 py-2 font-inter text-[16px] text-[#031136] hover:bg-gray-100">Edit Profile</span></Link>
+                    //         <Link to='/'><span href="#" className="block px-4 py-2 font-inter text-[16px] text-[#031136] hover:bg-gray-100" onClick={handleLogout}>Logout</span></Link>
+                    //     </div>
+                    // </div>
+                )}
+            </div>
+
   </div>
+}
+
 </nav>
     </div>
   )
