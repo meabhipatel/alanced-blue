1ee4af0e9a7a6910bbd16af864156106bf4ea1c0
sonam <sonamporwal.wiz91@gmail.com>
2023-09-11 18:16:14 +0530

    created Add language,Availability,Edit Education,Edit Hourly rate &  Edit language popup & changes in Edit title & add education popup

---
+++ src/container/freelancer/AllPopup/EditEducationPopup.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const EditEducationPopup = ({ qualification, university, startdate ,enddate,stream,closeEditEducation }) => {
+
+    const degrees = ["Bachelor's", "Master's", "PhD", "Associate", "Diploma"];
+
+  return (
+    <div className="fixed z-10 inset-0 overflow-y-auto mt-28">
+    <div className="fixed inset-0 bg-black opacity-50"></div>
+    <div className="flex items-center justify-center min-h-screen">
+    <div className="bg-white rounded-lg w-[90%] md:w-[61%] p-6 px-8 relative z-20">
+    <div className="flex justify-between items-center">
+        <h1 className="font-cardo text-[26px] text-[#031136] font-normal">Edit Education</h1>
+        <button onClick={closeEditEducation} className="text-gray-500 hover:text-gray-700">
+            <i class="bi bi-x-lg"></i>
+        </button>
+    </div>
+    <div className='mt-10'>
+            <h1 className="font-cardo text-[20px] text-[#031136] font-normal text-left">School/ University</h1>
+            <input type="text" className='border mt-2 mb-6 py-1.5 px-2 rounded-md w-full focus:border-lime-400 focus:outline-none focus:ring-1 focus:ring-lime-600' defaultValue={university}/>
+            <h1 className="font-cardo text-[20px] text-[#031136] font-normal text-left">Dates Attended</h1>
+            <div className="flex justify-between items-center mt-2 mb-6">
+        <select className="border py-2 px-2 rounded-md focus:border-lime-400 focus:outline-none focus:ring-1 focus:ring-lime-600 mr-5 flex-1 bg-white opacity-50" defaultValue={startdate}>
+            <option value="" disabled selected>{startdate}</option>
+            {Array.from({ length: new Date().getFullYear() - 1980 + 1 }, (_, idx) => 1980 + idx).map(year => (
+                <option key={year} value={year}>{year}</option>
+            ))}
+        </select>
+        <select className="border py-2 px-2 rounded-md focus:border-lime-400 focus:outline-none focus:ring-1 focus:ring-lime-600 flex-1 bg-white opacity-50" defaultValue={enddate}>
+            <option value="" disabled selected>{enddate}</option>
+            {Array.from({ length: new Date().getFullYear() - 1980 + 1 + 4 }, (_, idx) => 1980 + idx).map(year => (
+                <option key={year} value={year}>{year}</option>
+            ))}
+        </select>
+            </div>
+            
+            <h1 className="font-cardo text-[20px] text-[#031136] font-normal text-left">Degree</h1>
+            <select className="border mt-2 mb-6 py-2 px-2 rounded-md w-full focus:border-lime-400 focus:outline-none focus:ring-1 focus:ring-lime-600 bg-white opacity-50" defaultValue={qualification}>
+            <option value="" disabled selected>{qualification}</option>
+            {degrees.map(degree => (
+                <option key={degree} value={degree}>{degree}</option>
+            ))}
+        </select>
+            <h1 className="font-cardo text-[20px] text-[#031136] font-normal text-left">Area of Study (Optional) </h1>
+            <input type="text" className='border mt-2 mb-6 py-1.5 px-2 rounded-md w-full focus:border-lime-400 focus:outline-none focus:ring-1 focus:ring-lime-600' defaultValue={stream}/>
+            {/* <h1 className="font-cardo text-[20px] text-[#031136] font-normal text-left">Description (Optional)</h1>
+            <textarea name="" id="" cols="30" rows="5" className='border mt-2 mb-6 py-1.5 px-2 rounded-md w-full focus:border-lime-400 focus:outline-none focus:ring-1 focus:ring-lime-600'></textarea> */}
+            <div className="mt-8 flex justify-end">
+            <Link to=''><span class="inline-block text-sm px-4 py-[10px] bg-gradient-to-r from-[#00BF58] to-[#E3FF75] border rounded border-none text-white mr-3 font-semibold" >Save</span></Link>
+            <div class="p-0.5 inline-block rounded bg-gradient-to-b from-[#00BF58] to-[#E3FF75]" onClick={closeEditEducation}>
+                <Link to=''><button class="px-2 py-1 bg-white"><p class="bg-gradient-to-r from-primary to-danger bg-clip-text text-transparent font-semibold text-sm py-[4px] px-[8px]">Cancel</p></button></Link>
+            </div>     
+            </div>
+            </div>
+</div>
+    </div>
+</div>
+  )
+}
+
+export default EditEducationPopup
--- src/container/freelancer/AllPopup/EditTitlePopup.js
+++ src/container/freelancer/AllPopup/EditTitlePopup.js
@@ -4,20 +4,21 @@ import { Link } from 'react-router-dom'
 const EditTitlePopup = ({ closeEditTitle }) => {
   return (
     <>
-    <div className="fixed z-10 inset-0 overflow-y-auto mt-10">
+    <div className="fixed z-10 inset-0 overflow-y-auto mt-12">
                     <div className="fixed inset-0 bg-black opacity-50"></div>
                     <div className="flex items-center justify-center min-h-screen">
                     <div className="bg-white rounded-lg w-[90%] md:w-[50%] p-6 px-8 relative z-20">
                     <div className="flex justify-between items-center">
-                        <h1 className="font-cardo text-[26px] text-[#031136] font-normal">Edit Title</h1>
+                        <h1 className="font-cardo text-[26px] text-[#031136] font-normal">Edit About Me Section</h1>
                         <button onClick={closeEditTitle} className="text-gray-500 hover:text-gray-700">
                             <i class="bi bi-x-lg"></i>
                         </button>
                     </div>
-                    <div className='mt-10'>
-                            <h1 className="font-cardo text-[20px] text-[#031136] font-normal text-left">Your Title</h1>
-                            <p className="font-inter text-[14px] text-[#031136] font-normal pt-2 text-left opacity-50">Enter a single sentence description of your professional skills/experience (e.g. Expert Web Designer with Ajax experience) </p>
-                            <input type="text" className='border my-5 py-1.5 px-2 rounded-md w-full focus:border-lime-400 focus:outline-none focus:ring-1 focus:ring-lime-600' placeholder='Python Developer'/>
+                    <div className='mt-8'>
+                            <h1 className="font-cardo text-[20px] text-[#031136] font-normal text-left">Your Designation</h1>
+                            <input type="text" className='border my-2 py-1.5 px-2 rounded-md w-full focus:border-lime-400 focus:outline-none focus:ring-1 focus:ring-lime-600' placeholder='Python Developer'/>
+                            <h1 className="font-cardo text-[20px] text-[#031136] font-normal text-left pt-5">About You</h1>
+                          <textarea name="" id="" cols="30" rows="5" className='border mt-2 mb-6 py-1.5 px-2 rounded-md w-full focus:border-lime-400 focus:outline-none focus:ring-1 focus:ring-lime-600'></textarea> 
                             <div className="mt-8 flex justify-end">
                             <Link to=''><span class="inline-block text-sm px-4 py-[10px] bg-gradient-to-r from-[#00BF58] to-[#E3FF75] border rounded border-none text-white mr-3 font-semibold" >Save</span></Link>
                             <div class="p-0.5 inline-block rounded bg-gradient-to-b from-[#00BF58] to-[#E3FF75]" onClick={closeEditTitle}>
