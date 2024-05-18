cfc72691a2a21900a001186e6f514b890d6ff860
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-05-18 15:47:19 +0530

    chore removed navbar from Background component and replace hero background

--- src/components/Layout/Background.js
+++ src/components/Layout/Background.js
@@ -1,23 +1,35 @@
-import React from 'react'
-import background from '../images/alanced-back.png'
-import Navbar from './Navbar'
-import { Link } from 'react-router-dom'
+import React from "react";
+import heroBackground from "../../assets/hero_background.svg";
+import { Link } from "react-router-dom";
 
 const Background = () => {
   return (
     <>
-    <Navbar/>
-    <div className="h-screen bg-center bg-cover" style={{ backgroundImage: `url(${background})` }}>
-        
-        <div className='text-left pt-36 ml-[11.5vw] -mt-[1.5vh]'>
-          <h1 className='text-[#797979] text-lg font-semibold tracking-wider'><span className='text-[#031136] indent-4'>Popular:</span> Design Art Business Video Editing</h1>
-          <h6 className='text-[50px] font-cardo font-semibold leading-[67.7px] mt-2 text-[#031136]'>Find & Hire <br/> Top 3% of expert <br/> on Alanced</h6>
-          <h1 className='mt-6 text-[#797979] text-[16px] font-inter font-normal leading-[26px] mb-3.5'>With the largest professional creative community online, simply <br/> search through from our website</h1>
-          <Link to='/sign-up'><span class="inline-block text-sm px-4 py-[10px] lg:mt-0 bg-gradient-to-r from-[#0909E9] to-[#00D4FF] border rounded border-none text-white mr-2 font-semibold">Get Started</span></Link>
+      <div
+        className="h-[110vh] bg-cover"
+        style={{ backgroundImage: `url(${heroBackground})` }}
+      >
+        <div className="text-left pt-40 ml-[11.5vw] ">
+          <h1 className="text-[#797979] text-lg font-semibold tracking-wider">
+            <span className="text-[#031136] indent-4">Popular:</span> Design Art
+            Business Video Editing
+          </h1>
+          <h6 className="text-[50px] font-semibold leading-[67.7px] mt-2 text-[#031136]">
+            Find & Hire <br /> Top 3% of expert <br /> on Alanced
+          </h6>
+          <h1 className="mt-6 text-[#797979] text-[16px] font-normal leading-[26px] mb-3.5">
+            With the largest professional creative community online, simply{" "}
+            <br /> search through from our website
+          </h1>
+          <Link to="/sign-up">
+            <span class="inline-block text-sm px-4 py-[10px] lg:mt-0 bg-gradient-to-r from-[#0909E9] to-[#00D4FF] border rounded border-none text-white mr-2 font-semibold">
+              Get Started
+            </span>
+          </Link>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Background
+export default Background;
