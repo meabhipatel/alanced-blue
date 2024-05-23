fb87c67cccea67d9c6897ff2cf7be2b4841d4adf
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-05-23 18:16:51 +0530

    update import for HeroSearchBox

--- src/components/Layout/Home.js
+++ src/components/Layout/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Background from "./Background";
 import Footer from "./Footer";
-import Section from "./HomeSection";
+import HeroSearchBox from "./HeroSearchBox";
 import HomeSection1 from "./HomeSection1";
 import HomeSection3 from "./HomeSection3";
 import HomeSection2 from "./HomeSection2";
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <div>
       <Background />
-      <Section />
+      <HeroSearchBox />
       <HomeSection1 />
       <HomeSection2 />
       <HomeSection3 />
