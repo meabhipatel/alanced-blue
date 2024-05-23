75afec0af529a4b5f7067ac75276c89426f1cf08
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-05-23 15:20:12 +0530

    chore HeroSearchBox component made responsive

--- src/components/Layout/HeroSearchBox.jsx
+++ src/components/Layout/HeroSearchBox.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import search from "../images/SearchOutlined.png";
 import { Link } from "react-router-dom";
+import { IoMdSearch } from "react-icons/io";
 
 const Section = () => {
   const [searchType, setSearchType] = useState("Talent");
@@ -11,50 +11,44 @@ const Section = () => {
 
   return (
     <>
-      <section className="flex justify-center">
-        <div className="lg:w-[77vw] w-[95vw] bg-white -mt-[57px] p-2 lg:h-24 rounded-2xl lg:flex justify-center items-center shadow-lg">
-          <Link
-            to={{
-              pathname:
-                searchType === "Talent" ? "/view-all/freelancer" : "/projects",
-            }}
-            onClick={() => window.scroll(0, 0)}
-          >
-            <div className="flex flex-row items-center p-4 lg:border-r-2 lg:border-r-[#1C3865] cursor-text">
-              <img src={search} alt="search-icon" className="w-5 h-5" />
-              <p className="w-96 ml-2 lg:mr-48 font-inter text-base text-gray-500">
-                {searchType === "Talent"
-                  ? "Search for the best freelancers in one place."
-                  : "Finding the best freelance jobs according your skills "}
-              </p>
-            </div>
-          </Link>
-          <div className="">
-            <select
-              className="bg-gray-50 text-[#797979] text-sm font-inter font-normal rounded-lg block p-2.5 ml-5"
-              onClick={handleSearchTypeChange}
-            >
-              <option value="Talent">Talent</option>
-              <option value="Jobs">Jobs</option>
-            </select>
-          </div>
-          <div className="lg:ml-12 mt-2">
+      <div className="flex justify-center">
+        <div className="w-[90%] lg:w-[80%] -mt-[57px] p-2 md:h-24 rounded-2xl flex flex-col md:flex-row items-center gap-3 shadow-lg bg-white">
+          <div className="w-full md:w-[60%] flex justify-center md:block">
             <Link
               to={{
-                pathname:
-                  searchType === "Talent"
-                    ? "/view-all/freelancer"
-                    : "/projects",
+                pathname: searchType === "Talent" ? "/view-all/freelancer" : "/projects",
               }}
-              onClick={() => window.scroll(0, 0)}
             >
-              <button className="rounded h-12 w-28 lg:ml-6 font-semibold text-white bg-gradient-to-r from-[#0909E9] to-[#00D4FF]">
-                Search
-              </button>
+              <div className="flex flex-row items-center gap-3 p-4 md:border-r-2 border-r-[#1C3865] cursor-text">
+                <IoMdSearch className="w-5 h-5 text-[#797979]" />
+                <p className="text-xs lg:text-base text-gray-500">
+                  {searchType === "Talent"
+                    ? "Search for the best freelancers in one place."
+                    : "Finding the best freelance jobs according your skills "}
+                </p>
+              </div>
             </Link>
           </div>
+
+          <div className="w-full md:w-[40%] flex justify-center items-center gap-3">
+            <div>
+              <select className="w-28 h-12 bg-gray-100 text-[#797979] text-sm font-normal rounded-lg block p-2.5" onClick={handleSearchTypeChange}>
+                <option value="Talent">Talent</option>
+                <option value="Jobs">Jobs</option>
+              </select>
+            </div>
+            <div className="">
+              <Link
+                to={{
+                  pathname: searchType === "Talent" ? "/view-all/freelancer" : "/projects",
+                }}
+              >
+                <button className="w-28 h-12 rounded font-semibold text-white bg-gradient-to-r from-[#0909E9] to-[#00D4FF]">Search</button>
+              </Link>
+            </div>
+          </div>
         </div>
-      </section>
+      </div>
     </>
   );
 };
