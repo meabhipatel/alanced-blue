35a7dafd337ec1848368c2b0945e6decf585032b
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-05-23 18:53:39 +0530

    chore Footer component has been made responsive

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -49,7 +49,7 @@ const Footer = () => {
     <>
       <footer className="footer-1 bg-white py-8 sm:py-12">
         <div className="container mx-auto px-4">
-          <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 lg:px-20">
+          <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-5 ">
             <div class="p-4 text-left">
               <h5 class="text-xl font-bold mb-6">Company Info</h5>
               <ul class="list-none footer-links text-gray-500">
@@ -92,23 +92,26 @@ const Footer = () => {
                 </Link>
               </ul>
             </div>
-            <div class="p-4">
-              <div className="bg-[#F4F5F9] lg:h-[220px] lg:w-[350px] -mt-6 lg:-ml-[100px]">
+            <div class="p-4 col-span-2">
+              <div className="bg-[#F4F5F9] lg:h-[220px] lg:w-[350px] -mt-6 ">
                 <h5 class="text-xl font-bold pt-[22px] text-start ml-[23px]">Subscribe</h5>
-                <input
-                  type="email"
-                  className="mt-3 lg:-ml-[60px] p-2 text-sm focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
-                  placeholder="Email Address"
-                  name="email"
-                  onChange={onChange}
-                  value={addUserSub.email}
-                />
-                <button
-                  class="inline-block text-sm px-4 py-[12px] lg:mt-0 bg-gradient-to-r from-[#0909E9] to-[#00D4FF] border rounded border-none text-white font-semibold"
-                  onClick={AddUserSubscribe}
-                >
-                  <img src={arrow} alt="" />
-                </button>
+                <div className="w-full flex items-center px-2 mt-5">
+                  <input
+                    type="email"
+                    className="w-full h-10 p-2 text-sm focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+                    placeholder="Email Address"
+                    name="email"
+                    onChange={onChange}
+                    value={addUserSub.email}
+                  />
+                  <button
+                    class="w-12 h-10 inline-block text-sm px-4 py-[12px] lg:mt-0 bg-gradient-to-r from-[#0909E9] to-[#00D4FF] border rounded border-none text-white font-semibold"
+                    onClick={AddUserSubscribe}
+                  >
+                    <img src={arrow} alt="" />
+                  </button>
+                </div>
+
                 <p className="text-[14px] pt-3 text-left pl-6 opacity-50">
                   Hello, we are Lift Media. Our goal is to translate <br />
                   the positive effects from revolutionizing how <br /> companies engage with their clients & their <br /> team.
@@ -117,9 +120,9 @@ const Footer = () => {
             </div>
           </div>
 
-          <hr className="mx-4 my-6 sm:mx-14" />
+          <hr className="mx-4 my-6 sm:mx-5" />
 
-          <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4 mt-5 px-4 sm:px-[55px]">
+          <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4 mt-5 px-4 ">
             <div className="flex-1 flex justify-start p-4 pl-0 pt-1">
               <Link to="/">
                 <div class="md:w-1/6 flex items-center flex-shrink-0">
