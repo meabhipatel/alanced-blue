1322392c9cb0911265acbad7ad6085112684d92c
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-05-27 18:09:01 +0530

    fix footer responsiveness and navigation links

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -49,52 +49,53 @@ const Footer = () => {
     <>
       <footer className="footer-1 bg-white py-8 sm:py-12">
         <div className="container mx-auto px-4">
-          <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-5 ">
-            <div class="p-4 text-left">
-              <h5 class="text-xl font-bold mb-6">Company Info</h5>
-              <ul class="list-none footer-links text-gray-500">
+          {/* ---> footer navigations  */}
+          <div className="w-full grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-5">
+            <div className="p-1 md:p-4 text-left">
+              <h5 className="text-xl font-bold mb-6">Company Info</h5>
+              <ul className="list-none footer-links text-gray-500">
                 <Link to="/about-us">
-                  <li class="mb-2 hover:text-blue-600 hover:underline">About us</li>
+                  <li className="mb-2 hover:text-blue-600 hover:underline">About us</li>
                 </Link>
                 <Link to="/contact-us">
-                  <li class="mb-2 hover:text-blue-600 hover:underline">Contact us</li>
+                  <li className="mb-2 hover:text-blue-600 hover:underline">Contact us</li>
                 </Link>
                 <Link to="/safety-security" onClick={() => window.scroll(0, 0)}>
-                  <li class="mb-2 hover:text-blue-600 hover:underline">Safety & Security</li>
+                  <li className="mb-2 hover:text-blue-600 hover:underline">Safety & Security</li>
                 </Link>
               </ul>
             </div>
-            <div class="p-4 text-left">
-              <h5 class="text-xl font-bold mb-6">About</h5>
-              <ul class="list-none footer-links text-gray-500">
+            <div className="p-1 md:p-4 text-left">
+              <h5 className="text-xl font-bold mb-6">About</h5>
+              <ul className="list-none footer-links text-gray-500">
                 <Link to="/enterprises">
-                  <li class="mb-2 hover:text-blue-600 hover:underline">Enterprise </li>
+                  <li className="mb-2 hover:text-blue-600 hover:underline">Enterprise </li>
                 </Link>
                 <Link to="/FAQ">
-                  <li class="mb-2 hover:text-blue-600 hover:underline">FAQ</li>
+                  <li className="mb-2 hover:text-blue-600 hover:underline">FAQ</li>
                 </Link>
                 <Link to="/why-alanced">
-                  <li class="mb-2 hover:text-blue-600 hover:underline">Alanced Foundation</li>
+                  <li className="mb-2 hover:text-blue-600 hover:underline">Alanced Foundation</li>
                 </Link>
               </ul>
             </div>
-            <div class="p-4 text-left">
-              <h5 class="text-xl font-bold mb-6">Policies</h5>
-              <ul class="list-none footer-links text-gray-500">
+            <div className="p-1 md:p-4 text-left">
+              <h5 className="text-xl font-bold mb-6">Policies</h5>
+              <ul className="list-none footer-links text-gray-500">
                 <Link to="/terms">
-                  <li class="mb-2 hover:text-blue-600 hover:underline">Terms</li>
+                  <li className="mb-2 hover:text-blue-600 hover:underline">Terms</li>
                 </Link>
                 <Link to="/privacy-policy">
-                  <li class="mb-2 hover:text-blue-600 hover:underline">Privacy</li>
+                  <li className="mb-2 hover:text-blue-600 hover:underline">Privacy</li>
                 </Link>
                 <Link to="/cookies">
-                  <li class="mb-2 hover:text-blue-600 hover:underline">Cookies</li>
+                  <li className="mb-2 hover:text-blue-600 hover:underline">Cookies</li>
                 </Link>
               </ul>
             </div>
-            <div class="p-4 col-span-2">
-              <div className="bg-[#F4F5F9] lg:h-[220px] lg:w-[350px] -mt-6 ">
-                <h5 class="text-xl font-bold pt-[22px] text-start ml-[23px]">Subscribe</h5>
+            <div className="p-1 md:p-4 col-span-2">
+              <div className="w-full bg-[#F4F5F9] p-2 -mt-6">
+                <h5 className="text-xl font-bold pt-[22px] text-start ml-[23px]">Subscribe</h5>
                 <div className="w-full flex items-center px-2 mt-5">
                   <input
                     type="email"
@@ -105,7 +106,7 @@ const Footer = () => {
                     value={addUserSub.email}
                   />
                   <button
-                    class="w-12 h-10 inline-block text-sm px-4 py-[12px] lg:mt-0 bg-gradient-to-r from-[#0909E9] to-[#00D4FF] border rounded border-none text-white font-semibold"
+                    className="w-12 h-10 inline-block text-sm px-4 py-[12px] lg:mt-0 bg-gradient-to-r from-[#0909E9] to-[#00D4FF] border rounded border-none text-white font-semibold"
                     onClick={AddUserSubscribe}
                   >
                     <img src={arrow} alt="" />
@@ -121,53 +122,45 @@ const Footer = () => {
           </div>
 
           <hr className="mx-4 my-6 sm:mx-5" />
-
+          {/* ---> footer copyrights text  */}
           <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4 mt-5 px-4 ">
             <div className="flex-1 flex justify-start p-4 pl-0 pt-1">
               <Link to="/">
-                <div class="md:w-1/6 flex items-center flex-shrink-0">
+                <div className="md:w-1/6 flex items-center flex-shrink-0">
                   <img src={logo} alt="" />
-                  <span class="font-semibold text-[23px] tracking-widest ml-2 text-[#031136]">ALANCED</span>
+                  <span className="font-semibold text-[23px] tracking-widest ml-2 text-[#031136]">ALANCED</span>
                 </div>
               </Link>
             </div>
             <div className="flex-1 flex justify-center p-4 pt-1">
-              {/* <div class="flex space-x-4 w-full md:pl-20">
-      <Link to='/terms' >
-      <div class="flex-1 p-2 font-bold font-larsseit text-[14px] hover:text-blue-600 hover:underline">Terms</div>
-      </Link>
-      <Link to='/privacy-policy' >
-      <div class="flex-1 p-2 font-bold font-larsseit text-[14px] hover:text-blue-600 hover:underline">Privacy</div>
-      </Link>
-      <Link to='/cookies' >
-      <div class="flex-1 p-2 font-bold font-larsseit text-[14px] hover:text-blue-600 hover:underline">Cookies</div>
-      </Link>
-    </div> */}
               <p className="font-bold text-sm">
-                Copyrights © 2024 Alanced Proudly Created by{" "}
-                <a href="https://wiz91.com/" target="_blank">
+                Copyrights © 2024 Alanced <br /> Proudly Created by
+                <a href="https://wiz91.com/" target="_blank" className="hover:underline text-orange-600">
                   {" "}
-                  Wiz91 Technologies{" "}
+                  Wiz91 Technologies
                 </a>
               </p>
             </div>
             <div className="flex-1 flex justify-end p-4 pr-0 pt-1">
-              <div class="flex sm:justify-center xl:justify-start md:justify-end">
+              <div className="flex sm:justify-center xl:justify-start md:justify-end">
                 <a
-                  href="https://in.linkedin.com/"
-                  class="w-8 h-8 border-2 border-gray-400 rounded-full text-center py-1 text-gray-600 hover:text-white hover:bg-gradient-to-r from-[#0909E9] to-[#00D4FF] hover:border-[#6f7cf3]"
+                  href="https://in.linkedin.com/company/wiz91-technologies"
+                  target="blank(_blank)"
+                  className="w-8 h-8 border-2 border-gray-400 rounded-full text-center py-1 text-gray-600 duration-1000 hover:text-white hover:bg-gradient-to-r from-[#0909E9] to-[#00D4FF] hover:border-[#6f7cf3] "
                 >
                   <img src={linkedin} alt="" className="ml-2 mt-1" />
                 </a>
                 <a
                   href="https://www.facebook.com/"
-                  class="w-8 h-8 border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-gradient-to-r from-[#0909E9] to-[#00D4FF] hover:border-[#6f7cf3]"
+                  target="blank(_blank)"
+                  className="w-8 h-8 border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-gradient-to-r from-[#0909E9] to-[#00D4FF] hover:border-[#6f7cf3] duration-300"
                 >
                   <img src={fb} alt="" className="ml-2.5 mt-1" />
                 </a>
                 <a
                   href="https://twitter.com/"
-                  class="w-8 h-8 border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-gradient-to-r from-[#0909E9] to-[#00D4FF] hover:border-[#6f7cf3]"
+                  target="blank(_blank)"
+                  className="w-8 h-8 border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-gradient-to-r from-[#0909E9] to-[#00D4FF] hover:border-[#6f7cf3] duration-1000"
                 >
                   <img src={twitter} alt="" className="ml-2 mt-1.5" />
                 </a>
